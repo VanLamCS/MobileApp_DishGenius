40ab27d47fefe7b5a7822f40350813fc74f22206
VanLamCS <levanlam7602@gmail.com>
2023-12-21 19:30:15 +0700

    default cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger/dist';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
     .setTitle('Dish Genius')
